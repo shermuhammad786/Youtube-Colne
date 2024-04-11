1d5bc726b927ad8e2621a9c5179d54e1f4716874
shermuhammad786 <sherabr141@gmail.com>
2024-04-12 00:10:50 +0500

    change

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -4,10 +4,10 @@ import { createError } from "../error.js"
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.myToken;
     console.log(req.cookies, "request.cookies")
-    if (!token) return next(createError(401, "You are not authenticated!"))
+    if (!token) return next(createError(401, "You are not authenticated!"));
 
     jwt.verify(token, process.env.JWT, (err, user) => {
-        if (err) return next(createError(403, "Token is not valid!"))
+        if (err) return next(createError(403, "Token is not valid!"));
 
         req.user = user;
 
