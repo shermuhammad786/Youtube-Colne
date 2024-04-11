bc60efdfaf5e8478233d1a6c1f47e61b4e001c68
shermuhammad786 <sherabr141@gmail.com>
2024-04-12 00:39:54 +0500

    changing origin

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://uptight-cap-worm.cyclic.app',
     credentials: true // enable sending cookies cross-origin if needed
 }));
 
